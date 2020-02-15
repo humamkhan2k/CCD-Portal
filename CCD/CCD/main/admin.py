35ca09464d5697eb24b63147371616207cbb3ee1
DHAWAL812 <dhawalbadi@gmail.com>
2020-02-15 23:34:24 +0530

    UI improvements

--- CCD/CCD/main/admin.py
+++ CCD/CCD/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile,StudentsAnnouncement
+from .models import UserProfile,StudentsAnnouncement,PrivateAnnouncement
 # Register your models here.
 
 admin.site.register(UserProfile)
 admin.site.register(StudentsAnnouncement)
+admin.site.register(PrivateAnnouncement)
--- CCD/CCD/main/views.py
+++ CCD/CCD/main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 #from django.contrib.auth.forms import UserCreationForm
-from .forms import UserForm, ProfileForm, StudentsAnnouncmentForm
+from .forms import UserForm, ProfileForm, StudentsAnnouncementForm, PrivateAnnouncementForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
-from .models import StudentsAnnouncement
+from .models import StudentsAnnouncement,UserProfile, User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 # Create your views here.
@@ -22,11 +22,28 @@ def home(request):
 def portal(request):
     return render(request, 'home.html')
 
-class StudentsAnnouncmentview(CreateView):
-     form_class = StudentsAnnouncmentForm
+@login_required
+def logout_view(request):
+    print ("hii")
+    logout(request)
+    return render(request,'homepage.html')
+
+class StudentsAnnouncementview(CreateView):
+     form_class = StudentsAnnouncementForm
      template_name = 'Student_Announcement_create.html'
      success_url = reverse_lazy('portal')
+     
+class PrivateAnnouncementview(CreateView):
+     form_class = PrivateAnnouncementForm
+     template_name = 'Private_Announcement_create.html'
+     success_url = reverse_lazy('portal')
 
+def profile(request):
+    #s = User.username
+    user = request.user
+    obj = UserProfile.objects.get(user=user)
+    args = {'obj' : obj}
+    return render(request,'profile.html',args)
 
 def signup(request):
     if request.method == 'POST':
@@ -41,7 +58,7 @@ def signup(request):
             raw_password = user_form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('portal')
     else:
         user_form = UserForm()
         profile_form = ProfileForm()
